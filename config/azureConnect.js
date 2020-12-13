0d8fd3beed583efe3deba985aa42a877f7dbdb41
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-12 20:16:02 -0600

    testing a function.

--- config/azureConnect.js
+++ config/azureConnect.js
@@ -1,16 +1,16 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
-const openSesame = async () => {
-	const vault404 = process.env['VAULT404'];
-	const KVUri = 'https://'+ vault404 + '.vault.azure.net';
+const vault = async (env, key) => {
+	const vault = process.env[`${env}`];
+	const KVUri = 'https://'+ vault + '.vault.azure.net';
 
 	const credential = new DefaultAzureCredential();
 	const client = new SecretClient(KVUri, credential)
 
-	const secret = await client.getSecret('session');
+	const secret = await client.getSecret(`${key}`);
 
 	return secret.value
 }
 
-module.exports = openSesame;
+module.exports = vault;

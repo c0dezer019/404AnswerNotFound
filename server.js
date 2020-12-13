655d6ff8c3cad9f44490e35eab21d72322f9a664
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-13 11:37:46 -0600

    Wrapping everything in a timeout.

--- server.js
+++ server.js
@@ -53,68 +53,68 @@ setTimeout(() => {
                },
           })
      );
-}, 2500);
 
-app.use(flash());
+     app.use(flash());
 
-app.use(passport.initialize());
-app.use(passport.session());
+     app.use(passport.initialize());
+     app.use(passport.session());
 
-app.use((req, res, next) => {
-     res.locals.alerts = req.flash();
-     res.locals.currentUser = req.user;
-     next();
-});
-
-// Routes
-
-app.get('/', (req, res) => {
-     const locals = {
-          title: '404AnswersNotFound',
-          description: 'Where answers are not found, but found.',
-          style: '/css/home.css',
-          isUserLoggedIn: false,
-     };
-     if (req.user) {
-          locals.isUserLoggedIn = true;
-     } else {
-          locals.isUserLoggedIn = false;
-     }
-
-     db.question.findAll({ limit: 3 }).then(question => {
-          db.answer
-               .findAll({ limit: 3 })
-               .then(answer => {
-                    db.category
-                         .findAll()
-                         .then(category => {
-                              res.render('home', {
-                                   meta: locals,
-                                   questions: question,
-                                   answers: answer,
-                                   cat: category,
-                              });
-                         })
-                         .catch(err => {
-                              console.log(err);
-                         });
-               })
-               .catch(err => {
-                    console.log(err);
-               });
+     app.use((req, res, next) => {
+          res.locals.alerts = req.flash();
+          res.locals.currentUser = req.user;
+          next();
      });
-});
 
-app.use('/auth', require('./controllers/auth'));
-app.use('/inquire', require('./controllers/inquire'));
-app.use('/users', require('./controllers/profiles'));
+     // Routes
 
-var server = app.listen(process.env.PORT || 8000, () =>
-     console.log(
-          `🎧You're listening to the smooth sounds of port ${
-               process.env.PORT || 8000
-          }🎧`
-     )
-);
+     app.get('/', (req, res) => {
+          const locals = {
+               title: '404AnswersNotFound',
+               description: 'Where answers are not found, but found.',
+               style: '/css/home.css',
+               isUserLoggedIn: false,
+          };
+          if (req.user) {
+               locals.isUserLoggedIn = true;
+          } else {
+               locals.isUserLoggedIn = false;
+          }
 
-module.exports = server;
+          db.question.findAll({ limit: 3 }).then(question => {
+               db.answer
+                    .findAll({ limit: 3 })
+                    .then(answer => {
+                         db.category
+                              .findAll()
+                              .then(category => {
+                                   res.render('home', {
+                                        meta: locals,
+                                        questions: question,
+                                        answers: answer,
+                                        cat: category,
+                                   });
+                              })
+                              .catch(err => {
+                                   console.log(err);
+                              });
+                    })
+                    .catch(err => {
+                         console.log(err);
+                    });
+          });
+     });
+
+     app.use('/auth', require('./controllers/auth'));
+     app.use('/inquire', require('./controllers/inquire'));
+     app.use('/users', require('./controllers/profiles'));
+
+     var server = app.listen(process.env.PORT || 8000, () =>
+          console.log(
+               `🎧You're listening to the smooth sounds of port ${
+                    process.env.PORT || 8000
+               }🎧`
+          )
+     );
+     module.exports = server;
+     
+}, 2500);

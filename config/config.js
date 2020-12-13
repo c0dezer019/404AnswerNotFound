cda327f51af21fa961be45bee5d734f351e783e0
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-12 20:43:54 -0600

    config adjustment test

--- config/config.js
+++ config/config.js
@@ -1,27 +1,32 @@
 require('dotenv').config();
+const vault = require('./azureConnect')
 
+const database = vault('DB404', '404title')
+const password = vault('DB404', '404p');
+const server = vault('DB404', '404server');
+const username = vault('DB404', '404u');
 
 module.exports = {
   "development": {
-    "username": process.env.AZURE_USER,
-    "password": process.env.AZURE_PASS,
-    "database": process.env.AZURE_DB,
-    "host": process.env.AZURE_SERVER,
+    "username": username,
+    "password": password,
+    "database": database,
+    "host": server,
     "dialect": "mssql",
     "encrypt": "true"
   },
   "test": {
-    "username": process.env.AZURE_USER,
-    "password": process.env.AZURE_PASS,
+    "username": username,
+    "password": password,
     "database": process.env.AZURE_DB,
-    "host": process.env.AZURE_SERVER,
+    "host": server,
     "dialect": "mssql"
   },
   "production": {
-    "username": process.env.AZURE_USER,
-    "password": process.env.AZURE_PASS,
-    "database": process.env.AZURE_DB,
-    "host": process.env.AZURE_SERVER,
+    "username": username,
+    "password": password,
+    "database": database,
+    "host": server,
     "dialect": "mssql"
   }
 }
